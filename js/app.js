68b9258b15105d0f512bb3023c8e278dd37b222a
Anastasia D <nasty.dolgova@gmail.com>
2021-03-14 20:43:35 +0300

    3

--- js/app.js
+++ js/app.js
@@ -1,19 +1,121 @@
 "use strict";
 let mainNav = document.querySelector(".main-nav"),
-    menuBtn = document.querySelector(".main-nav__toogle");
+    menuBtn = document.querySelector(".main-nav__toogle"),
+    orderBtnIndex = document.querySelector('.info__btn'),
+    popUp = document.querySelector('.modal'),
+    closeModalBtn = document.querySelector('.modal__link-close');
+
 function openMenu() {
-    mainNav.classList.toggle("main-nav--closed"), bgDiv.classList.toggle("hidden"), menuBtn.classList.toggle("main-nav__toogle--close");
-}
-mainNav.classList.remove("main-nav--no-js"), menuBtn.addEventListener("click", openMenu);
-let prevBtn = document.querySelector(".button--prev"),
-    nextBtn = document.querySelector(".button--next"),
-    reviewItems = document.querySelectorAll(".slider__item");
-prevBtn.addEventListener("click", slidePrev), nextBtn.addEventListener("click", slideNext);
-let slideIndex = 0;
-function slideNext() {
-    reviewItems.forEach((e) => e.classList.add("hidden")), slideIndex < reviewItems.length - 1 ? slideIndex++ : (slideIndex = reviewItems.length - 1) && (slideIndex = 0), reviewItems[slideIndex].classList.remove("hidden");
-}
-function slidePrev() {
-    reviewItems.forEach((e) => e.classList.add("hidden")), 0 == slideIndex ? (slideIndex = reviewItems.length - 1) : 0 < slideIndex < reviewItems.length - 1 && slideIndex--, reviewItems[slideIndex].classList.remove("hidden");
+    mainNav.classList.toggle("main-nav--closed"),
+    menuBtn.classList.toggle("main-nav__toogle--close");
 }
 
+mainNav.classList.remove("main-nav--no-js"), menuBtn.addEventListener("click", openMenu);
+
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// Модалка
+
+orderBtnIndex.addEventListener('click', openModal ); 
+closeModalBtn.addEventListener('click', openModal ); 
+
+document.addEventListener("keydown", function (e) {
+    if ('Escape' === e.key && !popUp.classList.contains("hidden")) {
+      popUp.classList.add("hidden");
+
+    }
+  });
+
+function openModal(){
+    popUp.classList.toggle('hidden');
+}
+
+
+//Анимация 
+
+var isScrolling = false;
+ 
+window.addEventListener("scroll", throttleScroll, false);
+
+function throttleScroll(e) {
+  if (isScrolling == false) {
+    window.requestAnimationFrame(function() {
+      scrolling(e);
+      isScrolling = false;
+    });
+  }
+  isScrolling = true;
+}
+
+document.addEventListener("DOMContentLoaded", scrolling, false);
+
+var listItems = document.querySelectorAll("main section");
+
+function scrolling(e) {
+  for (var i = 0; i < listItems.length; i++) {
+    var listItem = listItems[i];
+
+    if (isPartiallyVisible(listItem)) {
+      listItem.classList.add("section--active");
+    } else {
+      listItem.classList.remove("section--active");
+    }
+  }
+}
+
+function isPartiallyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+  var height = elementBoundary.height;
+
+  return ((top + height >= 0) && (height + window.innerHeight >= bottom));
+}
+
+function isFullyVisible(el) {
+  var elementBoundary = el.getBoundingClientRect();
+
+  var top = elementBoundary.top;
+  var bottom = elementBoundary.bottom;
+
+  return ((top >= 0) && (bottom <= window.innerHeight));
+}
